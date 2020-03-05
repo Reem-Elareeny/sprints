80b7e396dfd138efc2d914f48983aca08ad3c30a
Reem Ahmed Gamal <reemel3reeny@gmail.com>
2020-03-05 03:30:52 +0200

    BCM V1

---
+++ BCM/MCU1_-MASTER/UART_BCM/BCM.h
@@ -0,0 +1,44 @@
+/*
+ * BCM.h
+ *
+ * Created: 3/2/2020 1:25:16 PM
+ *  Author: ahmed
+ */ 
+
+
+#ifndef BCM_H_
+#define BCM_H_
+
+#include "USART.h"
+#include "SPI.h"
+#include "BCM_LCfg.h"
+
+#define IDLE				(0xff)
+
+#define START_SENDING		(1)
+#define SENDING				(2)
+#define SENDING_COMPLETE	(3)
+
+#define START_RECEIVING		(4)
+#define RECEIVING			(5)
+#define RECEIVING_COMPLETE	(6)
+
+#define UART				(0)
+#define SPI					(1)
+#define SENDER				(2)
+#define RECEIVER			(3)
+
+#define INITIAL_VALUE				 0
+
+
+ERROR_STATUS BCM_Init(void);
+ERROR_STATUS BCM_DeInit(void);
+ERROR_STATUS BCM_RxDispatch(void);
+ERROR_STATUS BCM_TxDispatch(void);
+ERROR_STATUS BCM_Send (uint8_t* pu8_Data , uint16_t u16_BytesNumber);
+ERROR_STATUS BCM_Receive(uint8_t* pu8_Data);
+
+
+
+
+#endif /* BCM_H_ */
---
+++ BCM/MCU2_SLAVE/UART_BCM/Debug/SPI.d
@@ -0,0 +1,12 @@
+SPI.d SPI.o: .././SPI.c .././SPI.h .././std_types.h .././registers.h \
+ .././SPI_Config.h .././interrupt.h
+
+.././SPI.h:
+
+.././std_types.h:
+
+.././registers.h:
+
+.././SPI_Config.h:
+
+.././interrupt.h:
